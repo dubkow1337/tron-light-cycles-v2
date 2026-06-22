11e88ab5e9355311a34853930f7dd3352197c8df
REDRABBIT <dubkow14@gmail.com>
2026-06-22 20:56:10 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -80,7 +80,7 @@ function showVictory(name) {
     if (overlay) {
         overlay.innerText = `${name.toUpperCase()} ПОБЕДИЛ!`;
         overlay.classList.add('show');
-        setTimeout(() => overlay.classList.remove('show'), 2000);
+        setTimeout(() => overlay.classList.remove('show'), 4000);
     }
     
     if (typeof startFireworks === 'function') {
