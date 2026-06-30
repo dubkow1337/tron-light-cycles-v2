3e3ceccfb4b6129a370129e839d27ebb8b0983c4
REDRABBIT <dubkow14@gmail.com>
2026-06-30 15:57:35 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
-
+ 
 // ============================================================
 // ===== 1. СЦЕНА, КАМЕРА, РЕНДЕР =====
 // ============================================================
